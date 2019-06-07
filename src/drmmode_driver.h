49737a447f794ebd7b8a79d79f3c37f58dcf10bc
OtherCrashOverride <OtherCrashOverride@user.noreply.github.com>
2019-06-07 07:05:23 +0000

    Add ODROID-N2 support.

--- src/drmmode_driver.h
+++ src/drmmode_driver.h
@@ -106,6 +106,6 @@ extern struct drmmode_interface exynos_interface;
 extern struct drmmode_interface pl111_interface;
 extern struct drmmode_interface kirin_interface;
 extern struct drmmode_interface sti_interface;
-
+extern struct drmmode_interface meson_interface;
 
 #endif
